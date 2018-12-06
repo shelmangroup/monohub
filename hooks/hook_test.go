a5cf2ca73cb73b37d5744183703bfcbe537b6c21
Lars Sjöström <lars@radicore.se>
2018-12-06 12:02:28 +0100

    simple hook test

---
+++ hooks/hook_test.go
@@ -0,0 +1,58 @@
+package hooks
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+)
+
+func setupTestCase(t *testing.T) func(t *testing.T) {
+	t.Log("setup test case")
+	oldStdin := os.Stdin
+
+	return func(t *testing.T) {
+		t.Log("teardown test case")
+		os.Stdin = oldStdin
+	}
+}
+
+func TestRunPreRecevie(t *testing.T) {
+	cases := []struct {
+		name     string
+		content  []byte
+		expected string
+	}{
+		{"should pass", []byte("0000000000000000000000000000000000000000 4ea06c7022cc6ad23d2a62361a1935f49f123456 refs/heads/master"), ""},
+		{"should fail", []byte("4ea06c7022cc6ad23d2a62361a1935f49f5168b3 4ea06c7022cc6ad23d2a62361a1935f49f123456 refs/heads/master"), "exit status 128"},
+	}
+	teardownTestCase := setupTestCase(t)
+	defer teardownTestCase(t)
+
+	for _, tc := range cases {
+		tmpfile, err := ioutil.TempFile("", "test")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer os.Remove(tmpfile.Name()) // clean up
+
+		if _, err := tmpfile.Write(tc.content); err != nil {
+			log.Fatal(err)
+		}
+
+		if _, err := tmpfile.Seek(0, 0); err != nil {
+			log.Fatal(err)
+		}
+
+		os.Stdin = tmpfile
+		if err := RunHookPreReceive(); err != nil {
+			if err.Error() != tc.expected {
+				t.Errorf("RunHookPreReceive failed: %v", err)
+			}
+		}
+
+		if err := tmpfile.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
