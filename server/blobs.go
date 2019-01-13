ad0b3385ff3350cbf69d5c5e22d09325570aaa5f
Lars Sjöström <lars@radicore.se>
2019-01-13 13:21:59 +0100

    initial impl Blobs api

--- server/blobs.go
+++ server/blobs.go
@@ -2,16 +2,41 @@ package server
 
 import (
 	"context"
+	"encoding/base64"
+	"io/ioutil"
 
 	pb "github.com/shelmangroup/monohub/api"
 	log "github.com/sirupsen/logrus"
-	// "gopkg.in/src-d/go-git.v4"
-	// "gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
 	// "gopkg.in/src-d/go-git.v4/plumbing/object"
 )
 
 // Blobs return commit information
 func (s *Server) Blobs(ctx context.Context, req *pb.BlobRequest) (*pb.BlobResponse, error) {
 	log.WithField("context", ctx).Debug("Blobs called")
-	return nil, nil
+	r, err := git.PlainOpen(s.storage.Repo.Path)
+	if err != nil {
+		return nil, err
+	}
+	blob, err := r.BlobObject(plumbing.NewHash(req.Sha))
+	if err != nil {
+		return nil, err
+	}
+	br, err := blob.Reader()
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadAll(br)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.BlobResponse{
+		Content:  base64.StdEncoding.EncodeToString(data),
+		Encoding: "base64",
+		Size:     blob.Size,
+		Sha:      blob.Hash.String(),
+	}, nil
 }
